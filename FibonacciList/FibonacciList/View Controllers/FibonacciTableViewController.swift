5cd7e35faa9b29f5a8ed5fef0f80c7ec7c7c9f77
Killectro <killectro@gmail.com>
2016-08-06 15:21:06 -0400

    Add xcodeproj, fib list controller, and supporting files

---
+++ FibonacciList/FibonacciList/View Controllers/FibonacciTableViewController.swift
@@ -0,0 +1,47 @@
+//
+//  FibonacciTableViewController.swift
+//  FibonacciList
+//
+//  Created by DJ Mitchell on 8/6/16.
+//  Copyright © 2016 Killectro. All rights reserved.
+//
+
+import UIKit
+
+final class FibonacciTableViewController: UITableViewController {
+    // MARK: - Public properties
+
+    // MARK: - Private properties
+    private let cellIdentifier = "fibonacciCell"
+    // Lazy initialize number formatter
+    private let formatter: NSNumberFormatter = {
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = .DecimalStyle
+        return formatter
+    }()
+    private var cellsToDisplay = 100
+    private let fibonacci: (Int -> NSDecimalNumber) = memoize { (fibonacci: (Int -> NSDecimalNumber), n: Int) in
+        return (n < 2) ? NSDecimalNumber(decimal: NSNumber(long: n).decimalValue) : fibonacci(n - 1).decimalNumberByAdding(fibonacci(n - 2))
+    }
+}
+
+// MARK: - Table view data source
+extension FibonacciTableViewController {
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellsToDisplay
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
+        cell.textLabel?.text = formatter.stringFromNumber(fibonacci(indexPath.row))
+        return cell
+    }
+
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        // Emulate pagination since iOS table views don't have an option for first class infinite scrolling
+        if indexPath.row == cellsToDisplay - 1 {
+            cellsToDisplay += 100
+            tableView.reloadData()
+        }
+    }
+}
